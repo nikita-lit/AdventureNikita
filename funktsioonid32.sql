c3690f7d2dd5b0e1f2ca8de8fcd6b0c871736804
nikita-lit <nikitalitvinenko28@gmail.com>
2025-10-09 14:12:26 +0300

    funktsioonid32 update

--- funktsioonid32.sql
+++ funktsioonid32.sql
@@ -2,8 +2,21 @@
 SELECT * FROM DimEmployee; 
 
 --Tabelisisev‰‰rtusega funktsioon e Inline Table Valued function (ILTVF) koodin‰ide:
-CREATE FUNCTION fn_ILVF_GetEmployees()
+CREATE FUNCTION fn_ILTVF_GetEmployees()
 RETURNS TABLE AS
-	RETURN (SELECT EmployeeKey, FirstName, CAST(BirthDate AS DATE) AS DOB FROM DimEmployee);
+	RETURN (SELECT EmployeeKey, FirstName, CAST(BirthDate AS date) AS DOB FROM DimEmployee);
 
-SELECT * FROM fn_ILVF_GetEmployees();
+--  Mitme avaldisega tabeliv‰‰rtusega funktsioonid e multi-statement table valued function (MSTVF)
+CREATE FUNCTION fn_MSTVF_GetEmployees()
+RETURNS @Table TABLE (EmployeeKey INT, FirstName NVARCHAR(20), DOB date) AS
+BEGIN
+	INSERT INTO @Table
+	SELECT EmployeeKey, FirstName, CAST(BirthDate AS date)
+	FROM DimEmployee
+	RETURN
+END
+
+SELECT * FROM fn_ILTVF_GetEmployees();
+SELECT * FROM fn_MSTVF_GetEmployees();
+
+UPDATE fn_ILTVF_GetEmployees() SET FirstName = 'Sam1' WHERE EmployeeKey = 1;
